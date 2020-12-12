9aa5a7249eb26e45b624702fdfaf2019f6e6c005
Kai Wei Nan <kaiweinan@yahoo.com>
2020-12-12 01:25:45 -0500

    commit tree a76e1b194b002ad7b7a12aa922d219b9151da1d5

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,33 @@
-# Write your code here.
+
+def badge_maker(name)
+return "Hello, my name is #{name}." 
+end
+
+def batch_badge_creator(array)
+    phrase=[]
+    array.each do |name|
+    badges="Hello, my name is #{name}."
+    phrase<<badges
+    end
+    return phrase
+end
+def assign_rooms(array)
+    phrase=[]
+    rooms=1
+    array.each do |names|
+    num= "Hello, #{names}! You'll be assigned to room #{rooms}!"
+    phrase<<num
+        rooms +=1
+    end
+return phrase
+end
+
+def printer(array)
+    batch_badge_creator(array).each do |badge|
+      puts badge
+    end
+  
+    assign_rooms(array).each do |assign|
+      puts assign
+    end
+end
